20b6d0d81f03073c5411aee281ee4455f573ebf9
CNZW <2429671588@qq.com>
2024-10-01 18:31:44 +0800

    JavaScript

--- js/watermark.js
+++ js/watermark.js
@@ -1,31 +1,29 @@
 setInterval(function () {
     var C2W = `
-    <div id="CNZW-watermark">
-        Powered by
-        <a class="CNZW-link" href="https://cnzw.us.kg/" target="_blank">CNZW</a>
-        <style>
-            #CNZW-watermark {
-                font-size: 30px;
-                color: #00000030;
-                user-select: none;
-                position: fixed;
-                right: 10px;
-                bottom: 5px;
-                z-index: 2147483647;
-            }
+    Powered by
+    <a class="CNZW-link" href="https://cnzw.us.kg/" target="_blank">CNZW</a>
+    <style>
+        #CNZW-watermark {
+            font-size: 30px;
+            color: #00000030;
+            user-select: none;
+            position: fixed;
+            right: 10px;
+            bottom: 5px;
+            z-index: 2147483647;
+        }
 
-            @font-face {
-                font-family: 'CNZW-link';
-                src: url("https://cnzw.us.kg/ttf/MC.ttf");
-            }
+        @font-face {
+            font-family: 'CNZW-link';
+            src: url("https://cnzw.us.kg/ttf/MC.ttf");
+        }
 
-            .CNZW-link {
-                font-family: 'CNZW-link';
-                color: #00000030;
-                text-decoration: none;
-            }
-        </style>
-    </div>
+        .CNZW-link {
+            font-family: 'CNZW-link';
+            color: #00000030;
+            text-decoration: none;
+        }
+    </style>
     `;
     if (document.querySelector("#CNZW-watermark") == null) {
         document.querySelector("body").innerHTML += "<div id='CNZW-watermark'></div>";
