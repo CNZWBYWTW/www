de3c2b4405b414d35b3a1ce071225f4d5911b377
CNZW <2429671588@qq.com>
2024-10-04 20:20:57 +0800

    JavaScript

--- js/all.js
+++ js/all.js
@@ -50,3 +50,10 @@ function p() {
         _p();
     }
 }
+
+//
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
