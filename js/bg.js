31f2464087c37048a7dffa1f7188650292001677
CNZW <2429671588@qq.com>
2024-10-19 19:42:26 +0800

    JavaScript

--- js/bg.js
+++ js/bg.js
@@ -4,15 +4,15 @@ var tp = 1.2;
 setInterval(function () {
     if (innerWidth / innerHeight < tp) {
         if (new Date().getMonth() + 1 == 4 && new Date().getDate() == 1) {
-            document.querySelector('body').style = "background-image: url('/img/background/4-1-P.png'), url('https://raw.bgithub.xyz/CNZWBYWTW/www/main/img/background/4-1-P.png'), url('https://cdn.jsdelivr.net/gh/CNZWBYWTW/www/img/background/4-1-P.png');";
+            document.querySelector('body').style = "background-image: url('/img/background/4-1-P.png');";
         } else {
-            document.querySelector('body').style = "background-image: url('/img/CNZW/background-p.png'), url('https://raw.bgithub.xyz/CNZWBYWTW/www/main/img/CNZW/background-p.png'), url('https://cdn.jsdelivr.net/gh/CNZWBYWTW/www/img/CNZW/background-p.png');";
+            document.querySelector('body').style = "background-image: url('/img/CNZW/background-p.png');";
         }
     } else {
         if (new Date().getMonth() + 1 == 4 && new Date().getDate() == 1) {
-            document.querySelector('body').style = "background-image: url('/img/background/4-1-C.png'), url('https://raw.bgithub.xyz/CNZWBYWTW/www/main/img/background/4-1-C.png'), url('https://cdn.jsdelivr.net/gh/CNZWBYWTW/www/img/background/4-1-C.png');";
+            document.querySelector('body').style = "background-image: url('/img/background/4-1-C.png');";
         } else {
-            document.querySelector('body').style = "background-image: url('/img/CNZW/background.png'), url('https://raw.bgithub.xyz/CNZWBYWTW/www/main/img/CNZW/background.png'), url('https://cdn.jsdelivr.net/gh/CNZWBYWTW/www/img/CNZW/background.png');";
+            document.querySelector('body').style = "background-image: url('/img/CNZW/background.png');";
         }
     }
 });
