bf4c30bc059520ffb19cf013bc7571e488d0517c
CNZW <2429671588@qq.com>
2025-01-14 18:07:19 +0800

    JavaScript

--- js/all.js
+++ js/all.js
@@ -81,7 +81,7 @@ function getPi(pij) {
 }
 
 /* validateEmail */
-function check_Email(email) {
+function check_email(email) {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
